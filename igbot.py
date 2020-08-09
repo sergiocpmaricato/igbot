91f2b067a7fb123d085803cd34730fe1e97ceb50
Sérgio Cabral <sergiocpmaricato@icloud.com>
2020-08-09 11:56:17 -0300

    Testando outro modo de publicar

--- igbot.py
+++ igbot.py
@@ -79,10 +79,12 @@ class InstagramBot:
         print('')
         time.sleep(tempo_de_espera)
         publicar = driver.find_element_by_xpath("//button[@class='sqdOP yWX7d    y3zKF     ']")
-        publicar.send_keys(Keys.RETURN)
+        #publicar.send_keys(Keys.RETURN)
+        campo_comentario.send_keys(Keys.RETURN)
         time.sleep(3)
         try:
-            publicar.send_keys(Keys.RETURN)
+            #publicar.send_keys(Keys.RETURN)
+            campo_comentario.send_keys(Keys.RETURN)
         except:
             pass
         time.sleep(10) # Tempo de publicação
